a7b317de4802bb70c544250ab6aa4f833a2657f9
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-11-30 11:31:07 +0300

    added profile

--- dist/OnjaBook.e31bb0bc.js
+++ dist/OnjaBook.e31bb0bc.js
@@ -33892,12 +33892,14 @@ exports.default = _default;
 },{"react":"node_modules/react/index.js","react-router-dom":"node_modules/react-router-dom/esm/react-router-dom.js"}],"post.json":[function(require,module,exports) {
 module.exports = [{
   "id": 1606724219114,
+  "profile": "https://bit.ly/37m6KRs",
   "userName": "Cheyenne Hallie",
   "photo": "https://bit.ly/3fPpcpr",
   "legend": "If trust is broken, sorry means nothing😥",
   "like": 0
 }, {
   "id": 1606724248835,
+  "profile": "https://bit.ly/3mqLL6o",
   "userName": "delancy",
   "photo": "https://bit.ly/39uElLI",
   "legend": "It is a wonderful day",
