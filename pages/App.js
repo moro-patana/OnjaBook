6d576dbee59086f1f8691b74bda75abcdf6db581
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-11-30 10:51:47 +0300

    imported PostList

--- pages/App.js
+++ pages/App.js
@@ -1,10 +1,16 @@
 import React from "react"
 import { Switch, Route } from "react-router-dom"
 import Header from "../components/header"
+import PostList from "../components/postList"
 function App() {
     return (
         <div className="container">
             <Header/>
+            <Switch>
+                <Route path="/">
+                    <PostList/>
+                </Route>
+            </Switch>
         </div>
     )
 }
