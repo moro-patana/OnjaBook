7b4caa16e1f767b14d428589b5aa3944f1611fa7
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-11-30 11:24:13 +0300

    created a function to toggle the like button

--- context.js
+++ context.js
@@ -9,8 +9,21 @@ function ContextProvider({children}) {
     useEffect(() => {
         setPosts(PostData)
     },[])
+    function toggleLike(id) {
+        const likePost = posts.map(post => {
+            if (post.id === Number(id)) {
+                console.log(post.like);
+                return {
+                    ...post,
+                    like: post.like + 1
+                }
+            }
+            return { ...post };
+        })
+        setPosts(likePost)
+    }
      return (
-        <Context.Provider value={{posts}}>
+        <Context.Provider value={{posts, toggleLike}}>
             {children}
         </Context.Provider>
     )
