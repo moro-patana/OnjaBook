4f8ebd74e9e8f8e971284d769f1ea3d7384d62e7
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-12-02 16:12:52 +0300

    use useReducer instead of state

--- context.js
+++ context.js
@@ -1,61 +1,27 @@
-import React, { useEffect, useReducer, useState } from "react"
-const Context = React.createContext()
-import PostsData from "./post.json"
-
-
-function ContextProvider({children}) {
-    const [state, dispatch] = useReducer((state, action) => {
-        switch(action.type) {
-            case "POST_LIST": return {...state, postList: action.postList};
-            case "USER": return {...state, user: action.value}
-            case "ADD_NEW_POST":
-                return {
-                    ...state, postList: action.postList}
-                
-            
-        } 
-    }, {
-        postList: [],
-        user: {
-            id: 11111,
-            userName: "Hallie",
-            profile:"https://picsum.photos/100"
-        }
-        
-    });
+import React, { createContext, useReducer,useEffect } from 'react';
+import PostData from "./post.json"
+import UserData from "./user.json"
+const Context = createContext();
+function ContextProvider({ children }) {
+	const [state, dispatch] = useReducer(
+		(state, action) => {
+			switch (action.type) {
+                case "SET_POST": 
+                 return {...state, posts: action.value}
+                case "SET_USERS": 
+                return {...state, users: action.value}
+			}
+		},
+		{
+			posts: [],
+			users: [],
+			// userLoggedIn: '',
+		}
+    );
     useEffect(() => {
-        dispatch({type: "POST_LIST", postList: PostsData})
-    
-     }, [])
-     function editProfile(e) {
-        e.preventDefault()
-        const newUserProfile = {
-            id: Date.now(),
-            userName: e.target.name.value,
-            profile: e.target.profile.value
-        }
-        setUser(newUserProfile)
-        e.target.reset()
-
-    }
-
-    
-    function addComment(e) {
-     e.preventDefault()
-     const newComment = {
-         friend: user.userName,
-         friendProfile: user.profile,
-         commentText: e.target.comment.value
-     }
-     setPosts([...posts, newComment])
-
-    }
-    return (
-        <Context.Provider
-        value={{state, dispatch}}
-        >
-            {children}
-        </Context.Provider>
-    )
+        dispatch({type:"SET_POST", value: PostData})
+        dispatch({type:"SET_USERS", value: UserData})
+    }, [])
+	return <Context.Provider value={{ state, dispatch }}>{children}</Context.Provider>;
 }
-export {ContextProvider, Context}
+export { Context, ContextProvider };
