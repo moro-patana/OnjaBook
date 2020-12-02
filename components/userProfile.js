5a75cb6a3d29b2b365f7e0a032bc135ac1112d00
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-12-02 09:01:40 +0300

    worked on the edit profile to help the user edit their profile

--- components/userProfile.js
+++ components/userProfile.js
@@ -1,17 +1,25 @@
-import React from "react"
-
+import React, {useContext} from "react"
+import { Context } from "../context"
 function UserProfile() {
-    return (
+    const {user, setUser, editProfile} = useContext(Context)
+     return (
         <div className="user__profile">
-            <form>
+            <form onSubmit={editProfile}>
                 <h3>Options</h3>
                 <fieldset>
                  <label>UserName</label>
-                 <input type="text" placeholder="Type your username here"/>
+                 <input 
+                 type="text" 
+                 placeholder="Type your username here"
+                 name="name"
+                 />
                 </fieldset>
                 <fieldset>
                  <label>Profile Picture</label>
-                 <input type="text" placeholder="Paste a url here"/>
+                 <input type="url" 
+                 name="profile" 
+                 placeholder="Paste a url here"
+                 />
                 </fieldset>
                 <button className="save">Save</button>
 
