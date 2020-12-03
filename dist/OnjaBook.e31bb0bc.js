ee877ba587d2bd66c5a354b80c3ecede278c6f58
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-12-03 14:57:24 +0300

    renamed the post.json file and updated the array object

--- dist/OnjaBook.e31bb0bc.js
+++ dist/OnjaBook.e31bb0bc.js
@@ -34330,7 +34330,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65132" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60426" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
